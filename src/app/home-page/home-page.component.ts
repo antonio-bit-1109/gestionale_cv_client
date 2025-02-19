44e2ec3444404c4086cb93a86340d03d9a7ca460
antonio-bit-1109 <antonio.rizzuti@hotmail.com>
2025-02-19 14:44:19 +0100

    home page defautl

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CvService } from '../services/cv.service';
 import { HttpErrorResponse } from '@angular/common/http';
 import { AuthService } from '../services/auth.service';
+import { IRespListaCV } from '../utility/modelResponse/modelResp';
 
 @Component({
   selector: 'app-home-page',
@@ -9,16 +10,24 @@ import { AuthService } from '../services/auth.service';
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss',
 })
-export class HomePageComponent {
-  constructor(private cvService: CvService, private authService: AuthService) {
-    if (this.authService.isUserLoggedIn() !== null) {
-      //
+export class HomePageComponent implements OnInit {
+  public AllCvList: IRespListaCV | undefined;
 
+  constructor(private cvService: CvService, private authService: AuthService) {}
+
+  ngOnInit(): void {
+    if (this.authService.isUserLoggedIn() !== null) {
       const id_utente = this.authService.getIdUser();
       console.log(id_utente);
       this.cvService.getAllUserCvs(id_utente).subscribe({
-        next: (res) => {},
-        error: (err: HttpErrorResponse) => {},
+        next: (res: IRespListaCV) => {
+          this.AllCvList = res;
+          console.log(this.AllCvList);
+        },
+
+        error: (err: HttpErrorResponse) => {
+          console.error(err.error);
+        },
       });
 
       //
--- src/app/utility/modelResponse/modelResp.ts
+++ src/app/utility/modelResponse/modelResp.ts
@@ -18,3 +18,21 @@ export interface TOKEN_PAYLOAD {
   iat: number; //createdAt
   exp: number; // expireAt
 }
+
+export interface IRespListaCV {
+  message: string | null;
+  listaCV: Icv[];
+}
+
+export interface Icv {
+  titolo: string;
+  esperienze_Precedenti: string;
+  competenze: string;
+  istruzione: string;
+  lingueConosciute: string;
+  descrizioneGenerale: string;
+  created_at: Date;
+  updated_at: Date;
+  proprietarioCV: string;
+  Path_file_System_pdf: string;
+}
