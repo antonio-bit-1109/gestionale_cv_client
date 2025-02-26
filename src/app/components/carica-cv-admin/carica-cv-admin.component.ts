328e84ca552aad43c763012ccb56d87aa054af9a
antonio-bit-1109 <antonio.rizzuti@hotmail.com>
2025-02-26 10:57:07 +0100

    completamento caricamento cv da parte dell admin

--- src/app/components/carica-cv-admin/carica-cv-admin.component.ts
+++ src/app/components/carica-cv-admin/carica-cv-admin.component.ts
@@ -4,6 +4,8 @@ import { UserService } from '../../services/user.service';
 import { filter, map, Observable, take } from 'rxjs';
 import { IGetAllUsers, IUser } from '../../utility/modelResponse/modelResp';
 import { HttpErrorResponse } from '@angular/common/http';
+import { ToastService } from '../../services/toast.service';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-carica-cv-admin',
@@ -20,10 +22,18 @@ export class CaricaCvAdminComponent implements OnInit {
 
   listaUtenti: IUser[] | undefined;
   candidatoSelected: string | undefined;
+
+  lingue: string = '';
+  istruzione = '';
+  esperienze = '';
+  descrizione = '';
+  competenze = '';
+  titolo = '';
   constructor(
     private config: PrimeNGConfig,
     private messageService: MessageService,
-    private userService: UserService
+    private userService: UserService,
+    private toastService: ToastService
   ) {}
 
   ngOnInit(): void {
@@ -51,6 +61,107 @@ export class CaricaCvAdminComponent implements OnInit {
     console.log('sto inviando robe');
     console.log(this.candidatoSelected, 'id candidato selezionato');
     console.log(this.files, 'files');
+
+    // se nessun file risulta caricato, toast di errore
+    if (this.files.length <= 0 || this.files === undefined) {
+      this.toastService.show(
+        'error',
+        'devi caricare un file in formato PDF prima di poter proseguire.',
+        'upload file',
+        'toastCaricaCv',
+        2000
+      );
+      return;
+    }
+
+    // se nessun candidato è stato selezionato, toast di errore
+    if (!this.candidatoSelected) {
+      this.toastService.show(
+        'error',
+        'seleziona un utente a cui assegnare il curriculum.',
+        'upload file',
+        'toastCaricaCv',
+        2000
+      );
+      return;
+    }
+
+    // se i campi aggiuntivi del candidato non sono stati compilati, toast di errore.
+    this.checkIfDatiCandidatoFilled();
+
+    const form_data = new FormData();
+  }
+
+  // metodo per assicurarsi che tutti i dati necessari per il salvataggio del cv siano presenti.
+  private checkIfDatiCandidatoFilled(): boolean {
+    // se uno dei campi necessari alla compilazione del cv non viene riempito non si può proseguire
+    if (!this.lingue || this.lingue === '') {
+      this.toastService.show(
+        'error',
+        "inserisci le lingue conosciute dall'utente",
+        'upload file',
+        'toastCaricaCv',
+        2000
+      );
+      return false;
+    }
+
+    if (!this.istruzione || this.istruzione === '') {
+      this.toastService.show(
+        'error',
+        'inserisci il livello di istruzione del candidato',
+        'upload file',
+        'toastCaricaCv',
+        2000
+      );
+      return false;
+    }
+
+    if (!this.esperienze || this.esperienze === '') {
+      this.toastService.show(
+        'error',
+        'inserisci le precedenti esperienze effettuate dal candidato.',
+        'upload file',
+        'toastCaricaCv',
+        2000
+      );
+      return false;
+    }
+
+    if (!this.descrizione || this.descrizione === '') {
+      this.toastService.show(
+        'error',
+        'inserisci una descrizione per il candidato.',
+        'upload file',
+        'toastCaricaCv',
+        2000
+      );
+      return false;
+    }
+
+    if (!this.competenze || this.competenze === '') {
+      this.toastService.show(
+        'error',
+        'inserisci le competenze del candidato.',
+        'upload file',
+        'toastCaricaCv',
+        2000
+      );
+      return false;
+    }
+
+    if (!this.titolo || this.titolo === '') {
+      this.toastService.show(
+        'error',
+        'inserisci un titolo per il cv.',
+        'upload file',
+        'toastCaricaCv',
+        2000
+      );
+      return false;
+    }
+
+    return true;
   }
 
   choose(event, callback) {
